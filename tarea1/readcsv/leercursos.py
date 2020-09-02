70db3b107ff2bd3245ce9241d3bd0071f30b6e9c
Jose Piedra Venegas <josepive@MacBook-Air.local>
2020-09-01 21:56:57 -0600

    test leer curso

--- tarea1/readcsv/leercursos.py
+++ tarea1/readcsv/leercursos.py
@@ -1,66 +1,52 @@
-from datetime import datetime
-
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, date_format, udf
-from pyspark.sql.types import (DateType, IntegerType, FloatType, StringType,
-                               StructField, StructType, TimestampType)
+from pyspark.sql.types import (DateType, IntegerType, FloatType,
+                               StringType, StructField, StructType, TimestampType)
 
 
 spark = SparkSession.builder.appName("Read EstudiantesJoin").getOrCreate()
 
-csv_schema = StructType([StructField('Carnet', IntegerType()),
-                         StructField('Nombre', StringType()),
-                         StructField('Carrera', StringType()),
-                         ])
+estudiantes_schema = StructType([StructField('Carnet', IntegerType()),
+                                 StructField('Nombre', StringType()),
+                                 StructField('Carrera', StringType())])
 
 estudiantes_df = spark.read.csv("estudiantes.csv",
-                           schema=csv_schema,
-                           header=False)
+                                schema=estudiantes_schema,
+                                header=False)
 
-#estudiantes_df.show()
+# estudiantes_df.printSchema()
+# estudiantes_df.show()
 
-
-
-# Load separate file para los cursos
 cursos_schema = StructType([StructField('CodigoCurso', IntegerType()),
-                         StructField('Creditos', IntegerType()),
-                         StructField('Carrera', StringType()),
-                         ])
+                            StructField('Creditos', IntegerType()),
+                            StructField('NombreCurso', StringType())])
 
 cursos_df = spark.read.csv('cursos.csv',
-                          schema=cursos_schema,
-                          header=False)
+                           schema=cursos_schema,
+                           header=False)
 
-#cursos_df.printSchema()
-#cursos_df.show()
+# cursos_df.printSchema()
+# cursos_df.show()
 
-# Load separate file para las notas
 notas_schema = StructType([StructField('Carnet', IntegerType()),
-                         StructField('CodigoCurso', IntegerType()),
-                         StructField('Nota', FloatType()),
-                         ])
+                           StructField('CodigoCurso', IntegerType()),
+                           StructField('Nota', FloatType())])
 
 notas_df = spark.read.csv('notas.csv',
                           schema=notas_schema,
                           header=False)
 
-#notas_df.printSchema()
-#notas_df.show()
+# notas_df.printSchema()
+# notas_df.show()
 
-# ...and join to the aggregates
-'''
-joint_df = stats_df.join(names_df, stats_df.customer_id == names_df.id)
-joint_df.printSchema()
-joint_df.show()
-'''
-def join_dataframes(estudiantes_df,notas_df,cursos_df,estudiantes_head,notas_head,cursos_head):
 
-    joint_df = estudiantes_df.join(notas_df, estudiantes_df.Carnet == notas_df.Carnet)
-    #joint_df.printSchema()
-    #joint_df.show()
+def join_dataframes(estudiantes_df, notas_df, cursos_df, notas_head, cursos_head):
 
-    jointFinal_df = joint_df.join(cursos_df, joint_df.CodigoCurso == cursos_df.CodigoCurso)
-    #jointFinal_df.printSchema()
-    #jointFinal_df.show()   
+    joint_df = estudiantes_df.join(notas_df, on=notas_head)
+    # joint_df.printSchema()
+    # joint_df.show()
+
+    jointFinal_df = joint_df.join(cursos_df, on=cursos_head)
+    # jointFinal_df.printSchema()
+    # jointFinal_df.show()
 
     return jointFinal_df
--- tarea1/readcsv/test_leercursos.py
+++ tarea1/readcsv/test_leercursos.py
@@ -1,28 +1,47 @@
 from .leercursos import join_dataframes
 
 
-def test_one_column_join(spark_session):
-    estudiantes_data = [(12345,'Juan Perez','Sociologia'), (67890,'Laura Chinchilla','Comunicacion'), (23456,'Luisa Lane Sanchez','Biologia'), (23987,'Eduardo Soto Jimenez','Filologia'), (78219,'Warner Sanchez Saborio','Ing. Civil'), (91234,'Marco Alvarado Rojas','Ing. Electrica')]
-    
-    estudiantes_ds = spark_session.createDataFrame(estudiantes_data,['Carnet', 'Nombre', 'Carrera'])
-    
-    notas_data = [(12345,1001,80),(67890,1002,60),(23456,1003,85),(23987,1004,90),(78219,1005,75),(91234,1006,70)]
+def test_multicolumn_join(spark_session):
+    estudiantes_data = [(12345, 'Juan Perez', 'Sociologia'),
+                        (67890, 'Laura Chinchilla', 'Comunicacion'),
+                        (23456, 'Luisa Lane Sanchez', 'Biologia'),
+                        (23987, 'Eduardo Soto Jimenez', 'Filologia'),
+                        (78219, 'Warner Sanchez Saborio', 'Ing. Civil'),
+                        (91234, 'Marco Alvarado Rojas', 'Ing. Electrica')]
 
-    notas_ds = spark_session.createDataFrame(notas_data,['Carnet', 'CodigoCurso', 'Nota'])
+    estudiantes_ds = spark_session.createDataFrame(
+        estudiantes_data, ['Carnet', 'Nombre', 'Carrera'])
 
-    cursos_data = [(1001,4,'Int. a Sociologia'),(1002,2,'Comunicacion'),(1003,4,'Matematica General'),(1004,1,'Actividad Deportiva'),(1005,4,'Estatica'),(1006,4,'Circuicos en CC')] 
+    notas_data = [(12345, 1001, 80), (67890, 1002, 60), (23456, 1003, 85),
+                  (23987, 1004, 90), (78219, 1005, 75), (91234, 1006, 70)]
 
-    cursos_ds = spark_session.createDataFrame(cursos_data,['CodigoCurso', 'Creditos', 'Carrera'])
+    notas_ds = spark_session.createDataFrame(
+        notas_data, ['Carnet', 'CodigoCurso', 'Nota'])
 
+    cursos_data = [(1001, 4, 'Int. a Sociologia'), (1002, 2, 'Comunicacion'),
+                   (1003, 4, 'Matematica General'), (1004, 1, 'Actividad Deportiva'),
+                   (1005, 4, 'Estatica'), (1006, 4, 'Circuitos en CC')]
+
+    cursos_ds = spark_session.createDataFrame(
+        cursos_data, ['CodigoCurso', 'Creditos', 'NombreCurso'])
 
     estudiantes_ds.show()
     notas_ds.show()
     cursos_ds.show()
 
-    actual_ds = join_dataframes(estudiantes_ds, notas_ds, cursos_ds, ['Carnet'], ['Carnet'], ['CodigoCurso'])
+    actual_ds = join_dataframes(estudiantes_ds, notas_ds, cursos_ds,
+                                ['Carnet'], ['CodigoCurso'])
 
     expected_ds = spark_session.createDataFrame(
-        [(12345, 'Juan Perez', 'Sociologia', 12345,1,80.0,1,4,'Int. a Sociologia'),(67890, 'Laura Chinchilla', 'Comunicacion', 67890,2,60.0,2,2,'Comunicacion'),(23456,'Luisa Lane Sanchez','Biologia',23456,3,85.0,3,4,'Matematica General'),(23987,'Eduardo Soto Jimenez','Filologia','23987',4,90.0,4,1,'Actividad Deportiva'),(78219,'Warner Sanchez Sanchez','Ing. Civil',78219,5,75.0,5,4,'Estatica'),(91234,'Marco Alvarado Rojas','Ing. Electrica',91234,6,70.0,6,4,'Circuicos en CC')])
+        [(1002, 67890, 'Laura Chinchilla', 'Comunicacion', 60, 2, 'Comunicacion'),
+         (1005, 78219, 'Warner Sanchez Saborio', 'Ing. Civil', 75, 4, 'Estatica'),
+         (1001, 12345, 'Juan Perez', 'Sociologia', 80, 4, 'Int. a Sociologia'),
+         (1004, 23987, 'Eduardo Soto Jimenez',
+          'Filologia', 90, 1, 'Actividad Deportiva'),
+         (1006, 91234, 'Marco Alvarado Rojas',
+          'Ing. Electrica', 70, 4, 'Circuitos en CC'),
+         (1003, 23456, 'Luisa Lane Sanchez', 'Biologia', 85, 4, 'Matematica General')],
+        ['CodigoCurso', 'Carnet', 'Nombre', 'Carrera', 'Nota', 'Creditos', 'NombreCurso'])
 
 
     expected_ds.show()
